0ccfceff894574ccd1a665d22704ae76915f5b30
dhardydeveloper <da.hardy@duocuc.cl>
2026-09-23 16:38:31 -0300

    Delete js directory

--- js/auth_check.js
+++
@@ -1,5 +0,0 @@
-// Verificar si el usuario ha iniciado sesión como admin
-if (localStorage.getItem('isAdmin') !== 'true') {
-    alert('Acceso denegado. Debes iniciar sesión como Administrador.');
-    window.location.href = 'login.html';
-}
